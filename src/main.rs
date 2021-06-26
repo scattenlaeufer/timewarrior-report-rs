6bf0279638656e991a3fe3f67daf8a5a6b8e9db3
Björn Guth <b.guth@netcon-consulting.com>
2021-06-26 16:45:53 +0200

    Start working on a very basic report

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead, Read};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    for line in io::stdin().lock().lines() {
+        println!("{:?}", line.unwrap());
+    }
+    Ok(())
 }
