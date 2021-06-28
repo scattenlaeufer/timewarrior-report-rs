5920da7a3e88aff811c2a40cf401148a9d8a8936
Björn Guth <b.guth@netcon-consulting.com>
2021-06-28 15:10:47 +0200

    Create a custom error enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
+use std::fmt;
 use std::io::{self, BufRead};
 
-pub fn run() -> io::Result<()> {
+#[derive(Debug)]
+pub enum ReportError {
+    IO(String),
+}
+
+impl std::error::Error for ReportError {}
+
+impl fmt::Display for ReportError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ReportError::IO(e) => write!(f, "IOError: {}", e),
+        }
+    }
+}
+
+impl From<io::Error> for ReportError {
+    fn from(error: io::Error) -> Self {
+        ReportError::IO(error.to_string())
+    }
+}
+
+pub fn run() -> Result<(), ReportError> {
     let mut config = String::new();
     let mut sessions = String::new();
     let mut config_done = false;
     for line in io::stdin().lock().lines() {
-        let raw_line = line.unwrap();
+        let raw_line = line?;
         if raw_line == "" {
             config_done = true;
         } else {
